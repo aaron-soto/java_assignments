20d1de98dd01eb2445d487eb6498e256474bd4d9
Aaron Soto <aaron.m.soto1@gmail.com>
2021-04-05 11:36:42 -0700

    PythagoreanTheorem

---
+++ PythagoreanTheorem/PythagoreanTheorem.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+
+public class PythagoreanTheorem {
+    public static void main(String[] args) {
+        int legA = 5;
+        int legB = 5;
+
+        double hypotenuse = Math.sqrt(Math.pow(legA, 2) + Math.pow(legB, 2));
+        System.out.println("Hypotenuse: " + hypotenuse);
+    }
+    
+}
