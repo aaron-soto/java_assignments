a703169e11c30ca7e3cb3831fd3ca1282f722aed
Aaron Soto <aaron.m.soto1@gmail.com>
2021-04-05 12:38:13 -0700

    String Manipulation

--- w1/d1/StringManipulation/StringManipulator.java
+++ w1/d1/StringManipulation/StringManipulator.java
@@ -1,5 +1,18 @@
+import java.lang.*;
+
 public class StringManipulator {
     public String trimAndConcat(String word1, String word2) {
         return word1.trim() + word2.trim();
+    }
+    public int getIndexOrNull(String word, char letter) {
+        return word.indexOf(letter);
+    }
+    public int getIndexOrNull(String word, String searchWord) {
+        return word.indexOf(searchWord);
+    }
+    public String concatSubstring(String word, int startIdx, int endIdx, String secondString) {
+        String subStr = word.substring(startIdx, endIdx);
+        subStr += secondString;
+        return subStr;
     }
 }
