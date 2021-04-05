5b1a3054f9f08993b03a1a60b4f571fee7450c7b
Aaron Soto <aaron.m.soto1@gmail.com>
2021-04-05 11:14:57 -0700

    First Java Program

---
+++ FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,11 @@
+public class FirstJavaProgram{
+    public static void main(String[] args) {
+        String name = "Aaron Soto";
+        int age = 25;
+        String hometown = "Casa Grande, Az";
+
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("My home is " + hometown);
+    }
+}
